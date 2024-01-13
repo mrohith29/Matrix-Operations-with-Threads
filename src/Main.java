96a2c14058657deb31529b63d1b25799e4a555ec
mrohith29 <mariyalarohith29@gmail.com>
2024-01-13 14:34:11 +0530

    update UI

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,15 +7,52 @@ public class Main extends Thread {
     int[][] mat1;
     int[][] mat2;
     int[][] mat3;
-    static int row1, row2, col1, col2;
     int row, col, choice;
+
+    public static int[][] getMatrixInput(int rows, int cols, String matrixName) {
+        JDialog inputDialog = new JDialog();
+        inputDialog.setTitle(matrixName);
+        inputDialog.setModal(true);
+        inputDialog.setSize(300, 200);
+
+        JPanel inputPanel = new JPanel(new GridLayout(rows, cols));
+        inputDialog.add(inputPanel, BorderLayout.NORTH);
+
+        JTextField[][] fields = new JTextField[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                fields[i][j] = new JTextField();
+                inputPanel.add(fields[i][j]);
+            }
+        }
+
+        JButton saveButton = new JButton("Save input");
+        inputPanel.add(saveButton);
+        saveButton.setBackground(Color.GREEN);
+
+        int[][] matrix = new int[rows][cols];
+        saveButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                for (int i = 0; i < rows; i++) {
+                    for (int j = 0; j < cols; j++) {
+                        matrix[i][j] = Integer.parseInt(fields[i][j].getText());
+                    }
+                }
+                inputDialog.dispose();
+            }
+        });
+
+        inputDialog.setVisible(true);
+
+        return matrix;
+    }
 
     Main(int[][] A, int[][] B, int[][] C, int i, int j, int choice) {
         this.mat1 = A;
         this.mat2 = B;
         this.mat3 = C;
-        row = i;
-        col = j;
+        this.row = i;
+        this.col = j;
         this.choice = choice;
     }
 
@@ -30,14 +66,13 @@ public class Main extends Thread {
     }
 
     public void multiply() {
-        for (int k = 0; k < row2; k++) {
+        for (int k = 0; k < mat2.length; k++) {
             mat3[row][col] += mat1[row][k] * mat2[k][col];
         }
     }
 
     public void add() {
         mat3[row][col] = mat1[row][col] + mat2[row][col];
-        // System.out.println(mat3[row][col]);
     }
 
     public void subtract() {
@@ -45,161 +80,122 @@ public class Main extends Thread {
     }
 
     public static void main(String[] args) throws InterruptedException {
-
         JFrame frame = new JFrame("Matrix Operation");
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 200);
+        // frame.setSize(300, 200);
 
-        JPanel inputPanel = new JPanel(new GridLayout(4, 2));
-        frame.add(inputPanel, BorderLayout.CENTER);
+        JPanel inputPanel = new JPanel(new GridLayout(0, 1));
+        inputPanel.setBackground(Color.black);
+        frame.add(inputPanel, BorderLayout.NORTH);
 
         JLabel label1 = new JLabel("Matrix 1 rows:");
         JTextField textField1 = new JTextField();
         inputPanel.add(label1);
         inputPanel.add(textField1);
+        label1.setForeground(Color.WHITE);
+        textField1.setBackground(Color.gray);
 
-        JLabel label2 = new JLabel("Matrix 1 columns");
+        JLabel label2 = new JLabel("Matrix 1 columns:");
         JTextField textField2 = new JTextField();
         inputPanel.add(label2);
         inputPanel.add(textField2);
+        label2.setForeground(Color.WHITE);
+        textField2.setBackground(Color.gray);
 
         JLabel label3 = new JLabel("Matrix 2 rows:");
         JTextField textField3 = new JTextField();
         inputPanel.add(label3);
         inputPanel.add(textField3);
+        label3.setForeground(Color.WHITE);
+        textField3.setBackground(Color.gray);
 
         JLabel label4 = new JLabel("Matrix 2 columns:");
         JTextField textField4 = new JTextField();
         inputPanel.add(label4);
         inputPanel.add(textField4);
+        label4.setForeground(Color.WHITE);
+        textField4.setBackground(Color.gray);
+
+        String[] operations = { "Multiply", "Add", "Subtract" };
+        JComboBox operationsBox = new JComboBox(operations);
+        inputPanel.add(operationsBox);
 
         JButton saveButton = new JButton("Save input");
+        inputPanel.add(saveButton);
+        saveButton.setBackground(Color.GREEN);
 
-        final JTextField finalTextField1 = textField1; 
-        final JTextField finalTextField2 = textField2; 
-        final JTextField finalTextField3 = textField3;
-        final JTextField finalTextField4 = textField4;
+        JTextArea outputArea = new JTextArea();
+        frame.add(new JScrollPane(outputArea), BorderLayout.CENTER);
+
         saveButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String input1 = finalTextField1.getText(); 
-                String input2 = finalTextField2.getText();
-                String input3 = finalTextField3.getText();
-                String input4 = finalTextField4.getText();
+                int row1 = Integer.parseInt(textField1.getText());
+                int col1 = Integer.parseInt(textField2.getText());
+                int row2 = Integer.parseInt(textField3.getText());
+                int col2 = Integer.parseInt(textField4.getText());
 
-                int row1 = Integer.parseInt(input1);
-                int col1 = Integer.parseInt(input2);
-                int row2 = Integer.parseInt(input3);
-                int col2 = Integer.parseInt(input4);
-            }
-        });
+                int[][] mat1 = new int[row1][col1];
+                int[][] mat2 = new int[row2][col2];
+                int[][] mat3 = new int[row1][col2];
 
-        int[][] mat1 = new int[row1][col1];
-        int[][] mat2 = new int[row2][col2];
-        int[][] mat3 = new int[row1][col2];
+                Main[][] threads = new Main[row1][col2];
 
-        JPanel matrixPanel = new JPanel(new GridLayout(row1, col1));
-        frame.add(matrixPanel, BorderLayout.CENTER);
+                mat1 = getMatrixInput(row1, col1, "Matrix 1");
 
-        JTextField[][] matrixOneFields = new JTextField[row1][col1];
-        for (int i = 0; i<row1;i++) {
-            for(int j =0;j<col1; j++) {
-                matrixOneFields[i][j] = new JTextField();
-                matrixPanel.add(matrixOneFields[i][j]);
-            }
-        }
+                mat2 = getMatrixInput(row2, col2, "Matrix 2");
 
-        JTextField[][] matrixTwoFields = new JTextField[row2][col2];
-        for (int i = 0; i<row2;i++) {
-            for(int j =0;j<col2; j++) {
-                matrixTwoFields[i][j] = new JTextField();
-                matrixPanel.add(matrixTwoFields[i][j]);
-            }
-        }
+                String selectOperation = (String) operationsBox.getSelectedItem();
 
-        final JTextField[][] finalMatrixOneFields = matrixOneFields;
-        final JTextField[][] finalMatrixTwoFields = matrixTwoFields;
-        final int[][] finalMat1 = mat1; 
-        final int[][] finalMat2 = mat2;
-        JButton storeButton = new JButton("Store Matrix");
-        storeButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                for (int i = 0; i < row1; i++) {
-                    for (int j = 0; j < col1; j++) {
-                        String input = finalMatrixOneFields[i][j].getText();
-                        finalMat1[i][j] = Integer.parseInt(input); 
+                if ("Multiply".equals(selectOperation)) {
+                    for (int i = 0; i < row1; i++) {
+                        for (int j = 0; j < col2; j++) {
+                            threads[i][j] = new Main(mat1, mat2, mat3, i, j, 1);
+                            threads[i][j].start();
+                        }
+                    }
+                } else if ("Add".equals(selectOperation)) {
+                    for (int i = 0; i < row1; i++) {
+                        for (int j = 0; j < col2; j++) {
+                            threads[i][j] = new Main(mat1, mat2, mat3, i, j, 2);
+                            threads[i][j].start();
+                        }
+                    }
+                } else if ("Subtract".equals(selectOperation)) {
+                    for (int i = 0; i < row1; i++) {
+                        for (int j = 0; j < col2; j++) {
+                            threads[i][j] = new Main(mat1, mat2, mat3, i, j, 3);
+                            threads[i][j].start();
+                        }
                     }
                 }
 
                 for (int i = 0; i < row1; i++) {
-                    for (int j = 0; j < col1; j++) {
-                        String input = finalMatrixTwoFields[i][j].getText();
-                        finalMat2[i][j] = Integer.parseInt(input);
+                    for (int j = 0; j < col2; j++) {
+                        try {
+                            threads[i][j].join();
+                        } catch (InterruptedException ex) {
+                            ex.printStackTrace();
+                        }
                     }
                 }
+
+                StringBuilder output = new StringBuilder();
+                for (int i = 0; i < row1; i++) {
+                    for (int j = 0; j < col2; j++) {
+                        output.append(mat3[i][j]).append(" ");
+                    }
+                    output.append("\n");
+                }
+                outputArea.setText(output.toString());
             }
         });
 
-        matrixPanel.add(storeButton);
+        JLabel outputTag = new JLabel("Output :", SwingConstants.LEFT);
+        inputPanel.add(outputTag);
+        outputTag.setForeground(Color.RED);
+        outputTag.setBackground(Color.YELLOW);
 
-        String[] operations = {"Multiply", "Add", "Subtract"};
-        JComboBox operationsBox = new JComboBox();
-        inputPanel.add(operationsBox);
-        String selectOperation = (String) operationsBox.getSelectedItem();
-
-        Main[][] threads = new Main[row1][col2];
-
-        if ("Multiply".equals(selectOperation)) {
-            for (int i = 0; i < row1; i++) {
-            for (int j = 0; j < col2; j++) {
-                threads[i][j] = new Main(mat1, mat2, mat3, i, j, 1);
-                threads[i][j].start();
-            }
-        }
-
-        for (int i = 0; i < row1; i++) {
-            for (int j = 0; j < col2; j++) {
-                threads[i][j].join();
-            }
-        }
-        } else if ("Add".equals(selectOperation)) {
-            for (int i = 0; i < row1; i++) {
-            for (int j = 0; j < col2; j++) {
-                threads[i][j] = new Main(mat1, mat2, mat3, i, j, 2);
-                threads[i][j].start();
-            }
-        }
-
-        for (int i = 0; i < row1; i++) {
-            for (int j = 0; j < col2; j++) {
-                threads[i][j].join();
-            }
-        }
-        } else if ("Subtract".equals(selectOperation)) {
-            for (int i = 0; i < row1; i++) {
-            for (int j = 0; j < col2; j++) {
-                threads[i][j] = new Main(mat1, mat2, mat3, i, j, 3);
-                threads[i][j].start();
-            }
-        }
-
-        for (int i = 0; i < row1; i++) {
-            for (int j = 0; j < col2; j++) {
-                threads[i][j].join();
-            }
-        }
-        }
-
-        JTextArea outpuArea = new JTextArea();
-
-        StringBuilder output = new StringBuilder();
-        for (int i = 0; i < row1; i++) {
-            for (int j = 0; j < col2; j++) {
-                output.append(mat3[i][j]).append(" ");
-            }
-            output.append("\n");
-        }
-
-        outpuArea.setText(output.toString());
-        frame.add(new JScrollPane(outpuArea),BorderLayout.SOUTH);
+        frame.setVisible(true);
     }
 }
